d8204b03856c4729fe69fb3f41f0ca8e29f32d55
NamanCode213 <73067709+NamanCode213@users.noreply.github.com>
2021-02-12 19:00:46 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,27 @@
+class chain{
+    constructor(bodyA,pointB){
+    var a={
+        bodyA:bodyA,
+        pointB:pointB,
+        length:10,
+        stiffness:0.04
+        
+    }
+    this.pointB=pointB
+    this.sling=Constraint.create(a)
+    World.add(world,this.sling)
+    }
+    Fly(){
+     this.sling.bodyA=null   
+    }
+    display(){
+    if(this.sling.bodyA){
+
+    
+    var pa=this.sling.bodyA.position
+    var pb=this.pointB
+
+    line(pa.x,pa.y,pb.x,pb.y)
+    }}
+
+}
